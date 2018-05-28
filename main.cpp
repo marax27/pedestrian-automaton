9fd95bb93d865bce1ea3898f48e2afe6a786ae54
marax27 <kacpert314@gmail.com>
2018-05-28 08:56:28 +0200

    Test: read snapshot from file

--- main.cpp
+++ main.cpp
@@ -10,48 +10,23 @@
 #include <cmath>
 #include <cstdlib>
 
+#include "snapshot.h"
+
 using sim::fp_t;
 using sim::index_t;
+using std::cout;
 
 const index_t DIM = 100;
 
 int main(){
-	srand(time(NULL));
-	sim::Field<fp_t> field(DIM);
+	sim::Snapshot shot;
+	shot.readFromFile("inputs/rooms.map");
 
-	for(index_t y = 0; y != DIM; ++y)
-		for(index_t x = 0; x != DIM; ++x)
-			field(y, x) = 0.0;
-	field(50, 50) = 20.0;
-
-	for(int i = 0; i != 25; ++i){
-		std::stringstream ss;
-		ss << "dumps/" << i << ".bmp";
-		std::ofstream writer(ss.str(), std::ios::binary);
-		JiMP2::BMP bmp(DIM+2, DIM+2);
-
-		for(index_t y = 0; y != DIM; ++y)
-			for(index_t x = 0; x != DIM; ++x){
-				auto gg = field.at(y, x) * 256.0;
-				uint8_t g = gg > 255.0 ? 255 : (uint8_t)gg;
-				bmp.setPixel(x+1, y+1, g, g, g);
-			}
-		
-		writer << bmp;
-		writer.close();
-
-		// Update.
-		for(index_t y=0; y!=DIM; ++y)
-			for(index_t x=0; x!=DIM; ++x){
-				auto &p = field(y, x);
-				if(!p)
-					continue;
-				int dx = rand() % 3 - 1;
-				int dy = rand() % 3 - 1;
-				fp_t df = 0.125 * p;
-				p -= df;
-				field.at(y + dy, x + dx) += df;
-			}
-
-	}
+	cout << "Size: " << shot.dimension << 'x' << shot.dimension << '\n'
+	     << shot.exits.size() << " exits.\n"
+		 << shot.pedestrians.size() << " pedestrians.\n"
+		 << shot.walls.size() << " walls.\n";
+	
+	// for(const auto &i : shot.walls)
+		// cout << '{' << i.x << ", " << i.y << "}\n";
 }
